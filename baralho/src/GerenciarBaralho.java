ddcd04accc0f1159e18c02902c667518f04d75e9
x <x>
2026-09-15 21:33:47 -0300

    aula 1509 pjt Constructor mais complexo

---
+++ baralho/src/GerenciarBaralho.java
@@ -0,0 +1,6 @@
+public class GerenciarBaralho {
+    public static void main(String[] args) {
+        Baralho baralho = new Baralho();
+        baralho.mostrarBaralho();
+    }
+}
---
+++ baralho/src/model/Naipe.java
@@ -0,0 +1,8 @@
+package model;
+
+public enum Naipe {
+    ouro,
+    espada,
+    copas,
+    paus
+}
